62acb10e7c7970f223f238f19b7a0178573420dc
Chris Stead <cmstead@gmail.com>
2015-01-23 23:02:01 -0800

    Overhauled project to reflect a basic js-core template.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,71 +1,37 @@
+var cleanConfig = require('./grunt/clean.json'),
+    concatConfig = require('./grunt/concat.json'),
+    copyConfig = require('./grunt/copy.json'),
+    jshintConfig = require('./grunt/jshint.json'),
+    karmaConfig = require('./grunt/karma.json'),
+    uglifyConfig = require('./grunt/uglify.json');
+
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        compass: {
-            dist: {
-                options: {
-                    config: 'config.rb'
-                }
-            }
-        },
-        jshint: {
-            files: ["scripts/src/**"],
-            options: {}
-        },
-        karma: {
-            unit: {
-                configFile: './spec/karma.config.js'
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-                sourceMap: true,
-                sourceMapName: 'scripts/min/<%= pkg.name %>.map'
-            },
-            dist: {
-                files: {
-                    'scripts/min/<%= pkg.name %>.min.js': ['./scripts/src/**/*.js']
-                }
-            }
-        },
-        jsdoc : {
-            dist : {
-                src: ['./scripts/src/*.js', './spec/*.js'],
-                dest: 'doc'
-            }
-        },
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['test'],
-            options: {
-                reload: true
-            }
-        }
+
+        clean: cleanConfig,
+        concat: concatConfig,
+        copy: copyConfig,
+        jshint: jshintConfig,
+        karma: karmaConfig,
+        uglify: uglifyConfig
     });
 
     /* Load grunt task adapters */
 
-    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-karma');
 
     /* Register composite grunt tasks */
 
     grunt.registerTask('test', ['jshint', 'karma']);
-    grunt.registerTask('testwatcher', ['test', 'watch']);
-    grunt.registerTask('document', ['jsdoc'])
 
-    grunt.registerTask('buildcss', ['compass']);
-    grunt.registerTask('buildjs', ['uglify'])
-    grunt.registerTask('build', ['buildcss', 'test', 'buildjs', 'document']);
+    grunt.registerTask('buildjs', ['concat', 'uglify']);
+    grunt.registerTask('build', ['clean', 'test', 'buildjs', 'copy', 'test-build']);
 
-    //Special build to handle work in progress experiments.
-    //NOT FOR PRODUCTION. BYPASSESS LINTING AND TESTING. BUILDS NO DOCUMENTS
-    grunt.registerTask('buildrough', ['buildcss', 'buildjs'])
-
-    grunt.registerTask('default', ['testwatcher']);
+    grunt.registerTask('default', ['test']);
 };
